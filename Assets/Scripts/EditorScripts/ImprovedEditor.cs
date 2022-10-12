539df50dbf56aed85b64b68b58ba20c357448e61
William Whitehouse <williamswhitehouse@gmail.com>
2022-10-12 17:40:35 +0100

    Added load next scene button to main menu. For debug purposes when two NES controllers can't be detected

---
+++ Assets/Scripts/EditorScripts/ImprovedEditor.cs
@@ -0,0 +1,10 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+public class ImprovedEditor<T> : Editor
+  where T : Object
+{
+  protected T Target => (T)target;
+}
+#endif
--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,40 +5,41 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private TMP_Text[] playerStatusText = Array.Empty<TMP_Text>();
-    [SerializeField] private string playerActiveText = "Joined!";
-    [Space]
-    [SerializeField] private int nextSceneIndex = 1;
-    [SerializeField] private float nextSceneWaitTimer = 1.5f;
-    
+  [SerializeField] private TMP_Text[] playerStatusText = Array.Empty<TMP_Text>();
+  [SerializeField] private string playerActiveText = "Joined!";
+  [Space]
+  [SerializeField] private int nextSceneIndex = 1;
+  [SerializeField] private float nextSceneWaitTimer = 1.5f;
+  
+  private bool _loadingScene = false;
 
-    private bool _loadingScene = false;
+  private delegate IEnumerator LoadNextSceneDel();
+  private LoadNextSceneDel LoadNextScene;
+  
+  private void Awake()
+  {
+    LoadNextScene = __LoadNextScene;
+      Debug.Assert(playerStatusText.Length == PlayerInput.MaxPlayerCount, "Player Status Text length doesn't match max player count!");
 
-    private delegate IEnumerator LoadNextSceneDel();
-    private LoadNextSceneDel LoadNextScene;
+      // Force all players to disconnect on starting this scene so we can detect new players
+      PlayerInput.RemoveAllPlayers();
 
+      PlayerInput.OnPlayerJoined += OnPlayerJoined;
+      PlayerInput.StartSearchingForPlayers();
+  }
 
-    private void Awake()
-    {
-        
-
-        LoadNextScene = __LoadNextScene;
-        Debug.Assert(playerStatusText.Length == PlayerInput.MaxPlayerCount, "Player Status Text length doesn't match max player count!");
-
-        // Force all players to disconnect on starting this scene so we can detect new players
-        PlayerInput.RemoveAllPlayers();
-
-        PlayerInput.OnPlayerJoined += OnPlayerJoined;
-        PlayerInput.StartSearchingForPlayers();
-    }
-
-    private void Start()
-    {
-        AudioManager.Play("Theme");
-        UpdateUI();
-    }
+  private void Start()
+  {
+      AudioManager.Play("Theme");
+      UpdateUI();
+  }
+  
   private void OnDestroy()
   {
     PlayerInput.OnPlayerJoined -= OnPlayerJoined;
@@ -67,12 +68,44 @@ public class MainMenu : MonoBehaviour
       if (!playerActive) continue;
       playerStatusText[i].text = playerActiveText;
     }
-       
-    }
+  }
   
   private IEnumerator __LoadNextScene()
   {
     yield return new WaitForSeconds(nextSceneWaitTimer);
     SceneManager.LoadScene(nextSceneIndex);
   }
+  
+#if UNITY_EDITOR
+  /// <summary>
+  /// This is an editor only function! Do NOT call it from
+  /// normal gameplay code!
+  /// </summary>
+  public void EDITOR_LoadNextScene()
+  {
+    _loadingScene = true;
+    SceneManager.LoadScene(nextSceneIndex);
+  }
+#endif
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(MainMenu))]
+public class MainMenuEditor : ImprovedEditor<MainMenu>
+{
+  public override void OnInspectorGUI()
+  {
+    base.OnInspectorGUI();
+    
+    EditorGUILayout.Space();
+    
+    using (new EditorGUI.DisabledScope(!Application.isPlaying))
+    {
+      if (GUILayout.Button("Load Next Scene"))
+      {
+        Target.EDITOR_LoadNextScene();
+      }
+    }
+  }
+}
+#endif
